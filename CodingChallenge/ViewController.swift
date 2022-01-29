f846af882434fd6d20dc10e3b834a8b70e0fe8a7
José Jacobo Contreras Trejo <jacobo@blue.gen.in>
2022-01-29 16:29:54 +0100

    Initial Commit

---
+++ CodingChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodingChallenge
+//
+//  Created by José Jacobo Contreras Trejo on 29/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
